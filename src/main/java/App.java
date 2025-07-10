5c94beb668e72d4a46c0b41e7140247aa87f86ca
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-07-10 10:14:53 +0300

    всё переписано

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,8 +2,23 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
+        CRUDUtils.createUser("Alice");
+        CRUDUtils.createUser("Bob");
+
+        CRUDUtils.assignRoleToUser(1, 1); // Alice - admin
+        CRUDUtils.assignRoleToUser(1, 2); // Alice - editor
+        CRUDUtils.assignRoleToUser(2, 2); // Bob - editor
 
         List<Users> users = CRUDUtils.getUserData("SELECT * FROM users");
-        System.out.println(users);
+        for (Users user : users) {
+            System.out.println(user);
+        }
+
+        //изменение пользователя по id
+        //CRUDUtils.updateUserName(1, "Alicia");
+
+        //удаление пользователя по его id
+        //CRUDUtils.deleteUser(1);
+
     }
 }
